8b2dd27c01e6be0517ce77e945158edf268cd01f
Nikolai Ivanov <ya.pravda@gmail.com>
2018-03-30 14:35:43 +0300

    Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+if Post.count.zero?
+  10.times do
+    post = Post.create(
+      title: Faker::Company.name,
+      body: Faker::Hipster.paragraph,
+      created_by: 1)
+    5.times do
+      Comment.create(
+        name: Faker::Name.name ,
+        body: Faker::Hipster.paragraph,
+        published: true,
+        post_id: post.id )
+    end
+  end
+  puts "-----\n #{Post.count} Posts with #{Comment.count} comments, has created. \n-----"
+end
